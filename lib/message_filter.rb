d457efad9339f49442d7902c928c7244ca99ce25
Yuji Takaesu <taka.esu.user@gmail.com>
2014-08-01 18:19:33 +0900

    1st iteration spec refactoring point

--- lib/message_filter.rb
+++ lib/message_filter.rb
@@ -1,2 +1,9 @@
 class MessageFilter
+  def initialize(word)
+    @word = word
+  end
+
+  def detect?(text)
+    text.include?(@word)
+  end
 end
--- spec/message_filter_spec.rb
+++ spec/message_filter_spec.rb
@@ -1,4 +1,13 @@
 require_relative '../lib/message_filter'
 
 describe MessageFilter do
+  it 'detects message with NG word' do
+    filter = MessageFilter.new('foo')
+    expect(filter.detect?('hello from foo')).to eq true
+  end
+
+  it 'does not detect message without NG word' do
+    filter = MessageFilter.new('foo')
+    expect(filter.detect?('hello, world!')).to eq false
+  end
 end
