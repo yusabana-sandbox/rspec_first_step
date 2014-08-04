e42f1d8096fd57bf978dbc66e7859fb6ef667f1c
Yuji Takaesu <taka.esu.user@gmail.com>
2014-08-05 08:10:30 +0900

    refactor spec with using shared_examples

--- spec/message_filter_spec.rb
+++ spec/message_filter_spec.rb
@@ -1,8 +1,20 @@
 require_relative '../lib/message_filter'
 
-describe MessageFilter, 'with argument "foo"' do
-  subject { MessageFilter.new('foo') }
+describe MessageFilter do
+  shared_examples 'MessageFilter with argument "foo"' do
+    it { is_expected.to be_detect('hello from foo') }
+    it { is_expected.not_to be_detect('hello, world!') }
+  end
 
-  it { is_expected.to be_detect('hello from foo') }
-  it { is_expected.not_to be_detect('hello, world!') }
+  context 'with argument "foo"' do
+    subject { MessageFilter.new('foo') }
+    it_behaves_like 'MessageFilter with argument "foo"'
+  end
+
+  context 'with argument "foo", "bar"' do
+    subject { MessageFilter.new('foo', 'bar') }
+    it { is_expected.to be_detect('hello from bar') }
+    it_behaves_like 'MessageFilter with argument "foo"'
+  end
 end
+
