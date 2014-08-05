85c8d4f24202cb6f1c911bc64b9e9ddd999a290a
Yuji Takaesu <taka.esu.user@gmail.com>
2014-08-05 23:41:33 +0900

    impliments ng_words before refactor

--- spec/message_filter_spec.rb
+++ spec/message_filter_spec.rb
@@ -9,6 +9,7 @@ describe MessageFilter do
   context 'with argument "foo"' do
     subject { MessageFilter.new('foo') }
     it_behaves_like 'MessageFilter with argument "foo"'
+    it { expect(subject.ng_words).not_to be_empty }
   end
 
   context 'with argument "foo", "bar"' do
